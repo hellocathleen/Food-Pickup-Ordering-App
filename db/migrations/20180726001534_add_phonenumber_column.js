5d58aa4661b14e476a5655deed0892cc8b6e8a7f
HypnoMama <sarahhermsen@gmail.com>
2018-07-26 05:30:24 +0000

    commented out lines so this file would work

--- db/migrations/20180726001534_add_phonenumber_column.js
+++ db/migrations/20180726001534_add_phonenumber_column.js
@@ -1,2 +1,19 @@
-//empty file to trick migration directory 
+//empty file to trick migration directory
 //original file did not migrate and was messing up migration
+
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+      // knex.schema.table('users', function(table) {
+      //   // table.string('phone_number');
+      // })
+    ])
+  };
+
+  exports.down = function(knex, Promise) {
+    return Promise.all([
+      // knex.schema.table('users', function(table) {
+      //   table.dropColumn('phone_number')
+      // })
+    ])
+  };
