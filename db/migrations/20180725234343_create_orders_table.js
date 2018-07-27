2c6c2dc9e5cd3678aa95dbb88afb74fd1d46f73b
Cathleen Melendez <cathleen@melendez.com>
2018-07-27 00:23:40 +0000

    Added updated_at to orders

--- db/migrations/20180725234343_create_orders_table.js
+++ db/migrations/20180725234343_create_orders_table.js
@@ -6,6 +6,7 @@ exports.up = function(knex, Promise) {
     table.foreign('user_id').references('users.id');
     table.decimal('total_price');
     table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
     table.boolean('status').defaultTo(false);
     table.string('comments');
     table.integer('est_wait_time');
