9684a43722db44263b58bda9f0994d53096a18e0
HypnoMama <sarahhermsen@gmail.com>
2018-07-27 00:40:42 +0000

    added click function to add to cart - ready to create post to database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,13 +13,39 @@ $(document).ready(function() {
 
 
 
+  const $cartbtn = $('#cartbtn'); //modal button
 
-  const $cartbtn = $('#cartbtn');
 
-  $cartbtn.on('show.bs.modal', function() {
-  //   $('.modal .fade').show();
-    console.log('cart button clicked')
+  // $cartbtn.on('show.bs.modal', function() {
+  // //   $('.modal .fade').show();
+  //   console.log('cart button clicked')
+  //   //
+  // })
+
+  const $divCard = $('.card')
+  const $addToCart = $('.addCart')
+  // const $modal = $('.modal')
+  const $modalBody = $('.model-body ol')
+
+
+  $addToCart.on('click', function(){
+
+    const $name = $(this).siblings('.card-title');
+    const $price = $(this).siblings('#price').text().replace('$', '')
+
+    const itemId = $(this).attr('id').replace('addToCart', '');//string
+    const $itemAdded = $('<li>').text($name.text())
+    console.log($itemAdded.text())
+
+
   })
+
+  // const $closeBtn = $('#close');
+
+
+  // $closeBtn.on('click', function() {
+  //   $modal.modal('hide')
+  // })
 
   //on send order
 
