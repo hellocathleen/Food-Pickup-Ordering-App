58b003f90431d531aa50431eb6660ddce233baad
HypnoMama <sarahhermsen@gmail.com>
2018-07-29 01:25:19 +0000

    got order from site to send a textgit add server.js!

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
-const cookieSession = require('cookie-session');
 const Cookies     = require('js-cookie');
+
+
+const accountSid  = process.env.TWILIO_ACCOUNT_SID;
+const authToken   = process.env.TWILIO_AUTH_TOKEN;
+const client      = require('twilio')(accountSid, authToken);
+const messageResponse = require('twilio').twiml.MessagingResponse;
+const twilioNum   = +17784028085;
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -27,13 +33,7 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.use(cookieSession ({
-  name: 'session',
-  keys: ['secret-string']
 
-}));
-
-//
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -64,14 +64,78 @@ app.get("/", (req, res) => {
 
 });
 
+app.post("/api/users", (req, res) => {
+  console.log("we are in the route on server.js", req.body)
+  knex('users')
+   .insert(req.body)
+   .returning('id')
+   .then(function(id){
+      res.send(id)
+   })
+
+})
+
+
+
 app.post("/api/orders", (req, res) => {
+  knex('orders')
+  .insert(req.body)
+  .returning('id')
+  .then(function(orderId){
+    res.send(orderId)
+  })
 
 })
 
-app.post("/api/cart", (req, res) => {
+app.get("/api/orders", (req, res) => {
+   knex
+   .select("*")
+   .from("orders")
+   .then((results) => {
+     res.json(results);
+ });
+});
 
-  res.redirect('/')
+//send the order to the restaurant
+app.post("/api/twilioSend", (req, res) => {
+  console.log(req.body)
+  let name = req.body.name;
+  let phoneNum = req.body.phoneNumber
+  let orderItems = JSON.stringify(req.body.orderItems)
+  let totalPrice = req.body.totalPrice
+  client.messages.create({
+    to: phoneNum,
+    from: twilioNum,
+    body: `New Order received from:  ${name}, @ ${phoneNum}, ${orderItems}  Total Price: ${totalPrice}`
+  })
+  .then((message) => console.log(message.sid))
+  .done()
+  res.end()
 })
+
+// client.messages.create({
+//     to: +12506341714,
+//     from: twilioNum,
+//     body: "This is working!!"
+//   })
+//   .then((message) => console.log(message.sid))
+//   .done()
+
+
+
+// process.env.PHONE
+//
+
+app.post("/api/restaurantSend", (req, res) => {
+  const twiml = new messageResponse();
+
+  twiml.message(req.body);
+
+  res.end(twiml.toString())
+})
+
+
+
 
 // app.get("/api/cart", (req, res) => {
 //   let cartOrder = req.session.cart
